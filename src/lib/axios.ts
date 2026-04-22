4f825cd9952bdb0aa6f34ce37407c48210c4cd09
Ye Myat Hein <yemyathein67@gmail.com>
2026-04-22 11:54:05 +0630

    fix: axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? "http://localhost:3000/api" : "https://chatify-backend-5kpndifsc-yemyathein67gmailcoms-projects.vercel.app/api",
+    baseURL: import.meta.env.MODE === 'development' ? "http://localhost:3000/api" : "https://chatify-xi-seven.vercel.app/api",
     withCredentials: true,
 });
